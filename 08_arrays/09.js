62912e66aff21bd208f11e232eeabe17ccfe8058
bryansteele <bryan@steele101.com>
2023-01-06 18:07:57 -0500

    Adds completed exercised for Arrays chapter

---
+++ 08_arrays/09.js
@@ -0,0 +1,10 @@
+
+let numbers1 = [1, 3, 5, 7, 9, 11];
+let numbers2 = [];
+let numbers3 = [2, 4, 6, 8];
+
+console.log(`
+  ${numbers1.includes(3)}
+  ${numbers2.includes(3)}
+  ${numbers3.includes(3)}`
+)
