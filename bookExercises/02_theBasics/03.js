f1e392ebf860fc3a19b3e92995baadf4f31dbc96
Bryan Steele <46832583+bryansteele@users.noreply.github.com>
2025-01-19 18:27:40 -0500

    Adds unfinnished code for Ex 3

---
+++ bookExercises/02_theBasics/03.js
@@ -0,0 +1,8 @@
+console.log(typeof 'true');            // String
+console.log(typeof false);             // Boolean
+console.log(typeof 1.5);               // Number
+console.log(typeof 2);                 // Number
+console.log(typeof undefined);         // Undefined
+console.log(typeof ({ foo: 'bar' }));  // Object
+
+
