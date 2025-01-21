007755b9e7ccf9a3a233ddfb1a09941f3c9b16d6
bryansteele <bryan@steele101>
2025-01-21 00:45:27 -0500

    Adds completed ex. for inputOutput

---
+++ bookExercises/04_inputOutput/03_age.js
@@ -0,0 +1,9 @@
+let rl = require('readline-sync');
+let age = Number(rl.question('How old are you? '));
+
+console.log();
+console.log(`You are ${age} years old.`)
+console.log(`In 10 years, you will be ${age + 10} years old.`)
+console.log(`In 20 years, you will be ${age + 20} years old.`)
+console.log(`In 30 years, you will be ${age + 30} years old.`)
+console.log(`In 40 years, you will be ${age + 40} years old.`)
