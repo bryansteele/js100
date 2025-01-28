5b648bebbc9b5c42a75919a35a1e32f7754c3e89
Bryan Steele <46832583+bryansteele@users.noreply.github.com>
2025-01-28 12:00:09 -0500

    *

---
+++ bookExercises/06_flowControl/02.js
@@ -0,0 +1,17 @@
+// function evenOdd(num) {
+//   console.log(num % 2 === 0 ? 'even' : 'odd');
+// }
+
+// OR
+
+function evenOdd(number) {
+  if (number % 2 === 0) {
+    console.log('even');
+  } else {
+    console.log('odd');
+  }
+}
+
+evenOdd(3);
+evenOdd(8);
+evenOdd(0);
