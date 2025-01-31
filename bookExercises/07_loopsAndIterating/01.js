cdffa5796b887407b3f0d65820c391417355696e
Bryan Steele <46832583+bryansteele@users.noreply.github.com>
2025-01-31 13:03:42 -0500

    *

---
+++ bookExercises/07_loopsAndIterating/01.js
@@ -0,0 +1,8 @@
+let rl = require('readline-sync');
+let age = rl.question('How old are you? ');
+age = parseInt(age);
+console.log(`You are ${age} years old.`);
+
+for (let x = 10; x <= 40; x += 10) {
+  console.log(`In ${x} years, you will be ${age + x} years old.`);
+}
